58313538e986947caa5f7b98fadf99dd65174bcf
kongsachakyarith <kongsachakyarith@gmail.com>
2022-06-24 14:23:02 +0700

    Create and responsive sidbars

--- src/components/admin-component/AdminFootersComponent.js
+++ src/components/admin-component/AdminFootersComponent.js
@@ -4,7 +4,7 @@ import { colors } from "../../commons/colors/colors";
 export default function AdminFootersComponent() {
   return (
     <div className="mt-5 laptop:grid-cols-3">
-      <section className="text-sm body-font font-maven laptop:text-md desktop:text-lg">
+      <section className="text-sm body-font font-maven laptop:text-md desktop:text-lg laptop:ml-64   tablet:ml-64">
         <div
           className="bg-white px-2 tablet:px-4 py-2.5 text-white "
           style={styles}
