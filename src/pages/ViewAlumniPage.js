22ce039990afa0c4d9105a38dacf9ce33c3e4374
sangsokea <sangsokea109@gmail.com>
2022-07-07 09:46:14 +0700

    search with elasticsearch, fixed register message, handle isAuth sidebar route

--- src/pages/ViewAlumniPage.js
+++ src/pages/ViewAlumniPage.js
@@ -17,11 +17,10 @@ import axios from "axios";
 export default function ViewAlumniPage() {
   const [dropdown, setDropdown] = useState(false);
   const [username, setUsername] = useState();
-  const [elasticData, setElasticData] = useState([])
-  const [totalHits, setTotalHits] = useState(0)
-  const [from, setFrom] = useState(0)
-  const [size, setSize] = useState(10)
-  
+  const [elasticData, setElasticData] = useState([]);
+  const [totalHits, setTotalHits] = useState(0);
+  const [from, setFrom] = useState(0);
+  const [size, setSize] = useState(10);
 
   const navigate = useNavigate();
 
@@ -30,26 +29,24 @@ export default function ViewAlumniPage() {
   };
 
   const onSearch = (searchKey) => {
-    console.log(searchKey)
+    console.log(searchKey);
     var config = {
-      method: 'get',
+      method: "get",
       url: `http://18.142.237.124:9200/person/_search?pretty=true&q=*${searchKey}*&from=${from}&size=${size}`,
-      headers: { 
-        'Authorization': 'Basic ZWxhc3RpYzpLZHB2eFM5aVRaUzJkb0prQ0JYbw=='
-      }
+      headers: {
+        Authorization: "Basic ZWxhc3RpYzpLZHB2eFM5aVRaUzJkb0prQ0JYbw==",
+      },
     };
-    
+
     axios(config)
-    .then(({data}) => {
-      console.log("========Elatic search successful=========");
-      console.log(data?.hits?.total?.value);
-      setTotalHits(data?.hits?.value)
-      setElasticData(data?.hits?.hits)
-    })
-    .catch(function (error) {
-      console.log("========Elatic search error=========");
-      console.log(error);
-    });
+      .then(({ data }) => {
+        console.log(data?.hits?.total?.value);
+        setTotalHits(data?.hits?.value);
+        setElasticData(data?.hits?.hits);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   const [data, setData] = useState([
@@ -85,6 +82,11 @@ export default function ViewAlumniPage() {
     },
   ]);
 
+  const handleView =(item) => {
+    // localStorage.setItem
+    navigate("/sidebar/aboutMe", {state: {item}})
+  }
+
   React.useEffect(() => {
     let username = localStorage.getItem("username");
     setUsername(username);
@@ -93,7 +95,7 @@ export default function ViewAlumniPage() {
   return (
     <div className="desktop:container desktop:mx-auto mt-5 rounded-md shadow-lg body-font font-maven">
       <div className="grid grid-cols-4">
-        <div className="hidden laptop:block laptop:col-span-1">
+        {/* <div className="laptop:block laptop:col-span-1">
           <div className="flex flex-col w-auto h-screen px-4 py-8 overflow-y-auto border-r ">
             <div className="mb-5 shrink-0 flex flex-row ">
               <img src={alumni4} className="w-10 rounded-full" alt="Avatar" />
@@ -104,74 +106,74 @@ export default function ViewAlumniPage() {
             <hr className="mb-5"></hr>
             <p className="mt-8 mb-5 text-2xl font-bold">Search</p>
 
-            <form>
-              <div className="relative mb-5 focus:border-none">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg
-                    className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    ></path>
-                  </svg>
-                </div>
-                <input
-                  onChange={(e) => onSearch(e.target.value)}
-                  type="search"
-                  id="default-search"
-                  className="block w-full p-2 pl-10 text-lg border border-black rounded-lg focus:ring-blue-600 focus:ring-2 dark:bg-gray-50 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none "
-                  placeholder="Search students..."
-                  required
-                />
-              </div>
-            </form>
+           
           </div>
-        </div>
-        <div className="col-span-3 laptop:col-span-2 desktop:col-span-3 rounded-br-md desktop:block bg-slate-200 text-sm laptop:text-md desktop:text-lg">
-          <img src={view_alumni} alt="view alumni logo"></img>
-          <div className="p-5">
-            <div className="mt-5 mb-3 text-2xl font-bold">Results</div>
-
-            {/* card */}
-            <div className="grid grid-cols-1 gap-5 desktop:grid-cols-2">
-              {elasticData?.map(({_source: {object: item}}, index) => (
-                <div className="w-full">
-                  <div className="desktop:flex-row p-0 flex items-center laptop:p-2 laptop:pl-5 border rounded-lg bg-gray-50 tablet:flex-row hover:bg-gray-100 hover:rounded-lg hover:shadow-md">
-                    <img
-                      className="w-24 h-24 rounded-full"
-                      src={item?.personalDetails?.profile}
-                      alt="alumni"
-                    />
-                    <div className="flex flex-col justify-between mt-5 ml-5 leading-normal">
-                      <h5 className="mb-2 text-md laptop:text-lg desktop:text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
-                        {item?.personalDetails?.firstName}
-                      </h5>
-                      <p className="text-sm laptop:text-md desktop:text-lg mb-7 font-normal text-black textLine w-52">
-                      {item?.personalDetails?.lastName}
-                      </p>
-                    </div>
-                    <button
-                      onClick={() => navigate("/sidebar/aboutMe")}
-                      style={{ backgroundColor: colors.footer, color: "white" }}
-                      className="ml-[-70px] mb-1 mt-auto rounded pl-5 pr-5 laptop:ml-auto"
-                    >
-                      View
-                    </button>
-                  </div>
-                </div>
-              ))}
+        </div> */}
+      </div>
+      <div className="col-span-3 laptop:col-span-2 desktop:col-span-3 rounded-br-md desktop:block bg-slate-200 text-sm laptop:text-md desktop:text-lg">
+        <img src={view_alumni} alt="view alumni logo"></img>
+        <div className="p-5">
+          <div className="mt-5 mb-3 text-2xl font-bold">Results</div>
+          <form>
+            <div className="relative mb-5 focus:border-none desktop::w-1/3 laptop:w-1/3 tablet:w-1/2 w-1/2">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                onChange={(e) => onSearch(e.target.value)}
+                type="search"
+                id="default-search"
+                className="block w-full p-2 pl-10 text-lg border border-black rounded-lg focus:ring-blue-600 focus:ring-2 dark:bg-gray-50 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none "
+                placeholder="Search students..."
+                required
+              />
             </div>
+          </form>
+          {/* card */}
+          <div className="flex flex-wrap overflow-auto">
+            {elasticData?.map(({ _source: { object: item } }, index) => (
+              <div className="desktop:w-1/2 laptop:w-1/2 tablet:w-full flex-1 mt-3 mx-2 ">
+                <div className="desktop:flex-row p-0 flex items-center laptop:p-2 laptop:pl-5 border rounded-lg bg-gray-50 tablet:flex-row hover:bg-gray-100 hover:rounded-lg hover:shadow-md">
+                  <img
+                    className="w-24 h-24 rounded-full"
+                    src={item?.personalDetails?.profile}
+                    alt="alumni"
+                  />
+                  <div className="flex flex-col justify-between mt-5 ml-5 leading-normal w-full">
+                    <h6 className="mb-2 text-md laptop:text-lg desktop:text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+                      {item?.personalDetails?.firstName}{" "}
+                      {item?.personalDetails?.lastName}
+                    </h6>
+                    <p className="text-sm laptop:text-md desktop:text-lg mb-7 font-normal text-black textLine w-52">
+                      {item?.personalDetails?.summary? item?.personalDetails?.summary: "No Summary"}
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => handleView(item)}
+                    style={{ backgroundColor: colors.footer, color: "white" }}
+                    className="ml-[-70px] mb-1 mt-auto rounded pl-5 pr-5 laptop:ml-auto"
+                  >
+                    View
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-
       <PaginationComponent />
     </div>
   );
